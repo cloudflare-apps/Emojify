a31deaaaf780dc01ab08fc90fb88d10d78666a81
Adam Schwartz <adam.flynn.schwartz@gmail.com>
2015-08-13 16:12:00 -0400

    Initial commit

---
+++ eager-emojione.js
@@ -0,0 +1,77 @@
+(function(){
+  if (!window.emojione) {
+    return;
+  }
+
+  emojione.imageType = 'svg';
+
+  if (!Array.prototype.indexOf) {
+    return;
+  }
+
+  maxLinksPerNode = 9999;
+  doNotTraverseTheseElements = ['script', 'br', 'col', 'command', 'hr', 'img', 'input', 'keygen', 'link', 'meta', 'param', 'source', 'track', 'wbr', 'video', 'iframe'];
+
+  var EagerEmojiOne = {
+    init: function(selector) {
+      var node;
+
+      try {
+        node = document.querySelector('');
+      } catch (e) {
+        node = document.body;
+      }
+
+      EagerEmojiOne.process(node);
+    },
+    process: function(node) {
+      if (node.nodeType === 3) {
+        var text = node.textContent;
+
+        // To get all `<img>`s, use this:
+        var emojiOnedText = emojione.unicodeToImage(emojione.shortnameToUnicode(text));
+
+        // To get all `<object>`s, use this:
+        // var emojiOnedText = emojione.shortnameToImage(emojione.toShort(text));
+
+        // To get all `<object>`s for :shortcode:s and `<img>` for unicodes, use this:
+        // var emojiOnedText = emojione.toImage(text);
+
+        if (emojiOnedText !== text) {
+          var fragment = document.createDocumentFragment();
+          var dummyElement = document.createElement('div');
+
+          dummyElement.innerHTML = emojiOnedText;
+
+          var loops = 0;
+          while (dummyElement.childNodes.length) {
+            loops += 1;
+            if (loops > maxLinksPerNode) {
+              break;
+            }
+
+            try {
+              fragment.appendChild(dummyElement.childNodes[0]);
+            } catch (e) {}
+          }
+
+          node.parentNode.replaceChild(fragment, node);
+        }
+      } else {
+        for (var i = 0; i < node.childNodes.length; i++) {
+          var child = node.childNodes[i];
+
+          if (!child.tagName || doNotTraverseTheseElements.indexOf(child.tagName.toLowerCase()) === -1) {
+            EagerEmojiOne.process(child);
+          }
+        }
+      }
+    }
+  };
+
+  window.EagerEmojiOne = EagerEmojiOne;
+
+  document.addEventListener('DOMContentLoaded', function(){
+    EagerEmojiOne.init();
+  });
+})();
